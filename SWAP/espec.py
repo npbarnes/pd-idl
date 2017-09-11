9be530de0c784fa9dc36ba425eb7d830a6ac78f7
Nathan Barnes <npbarnes@alaska.edu>
2017-09-11 09:20:55 -0800

    V4 espec

--- SWAP/espec.py
+++ SWAP/espec.py
@@ -7,6 +7,7 @@ from sys import argv
 from numpy.ma import masked_array
 from numpy import logical_and
 from mom_trans_pluto import mom_trans
+import numpy as np
 
 def _espec_v1(data):
     x = data['xpos']
@@ -108,16 +109,65 @@ def _espec_v3(data, options=None):
 
     return f, ax
 
+def _espec_v4(data, options=None):
+    if options is None:
+        options= {'colorbars':['Blues','Greens','Reds']}
+
+    spectrograms_by_species = data['spectrograms_by_species']
+    t = data['times']
+    x = data['positions'][0,:]/1187
+    o = data['orientations']
+    ebins = data['ebins']
+
+    H = spectrograms_by_species[:,:,0]
+    He = spectrograms_by_species[:,:,1]
+    CH4 = spectrograms_by_species[:,:,2]
+
+    cnt_arr = H+He+CH4
+
+    mH = masked_array(cnt_arr, mask=(~logical_and(H>He,H>CH4)))
+    mHe = masked_array(cnt_arr, mask=(~logical_and(He>H,He>CH4)))
+    mCH4 = masked_array(cnt_arr, mask=~(CH4>0))
+
+    f = plt.gcf()
+    ax = plt.gca()
+
+    norm = LogNorm()
+
+    Hhist = ax.pcolormesh(x, ebins, mH, norm=norm, cmap=options['colorbars'][0])
+    Hcb = plt.colorbar(Hhist, fraction=0.075, pad=-0.035, shrink=0.75)
+    Hcb.ax.minorticks_on()
+
+    Hehist = ax.pcolormesh(x, ebins, mHe, norm=norm, cmap=options['colorbars'][1])
+    Hecb = plt.colorbar(Hehist, fraction=0.075, pad=-0.033, shrink=0.75, format="")
+
+    CH4hist = ax.pcolormesh(x, ebins, mCH4, norm=norm, cmap=options['colorbars'][2])
+    CH4cb = plt.colorbar(CH4hist, fraction=0.075, pad=0.025, shrink=0.75, format="")
+
+    Hecb.ax.set_title('COIN (Hz)', fontdict={'fontsize':'small'})
+    ax.set_yscale('log')
+    ax.set_ylabel('Energy per charge ($eV/q$)')
+    ax.set_xlabel('X position ($R_p$)')
+    ax.invert_xaxis()
+
+    return f, ax
+
+
 def makeespec(name, options=None):
     data = readsav(name+".sav")
     try:
-        return _espec_v3(data, options)
+        return _espec_v4(data, options)
     except KeyError:
         try:
-            return _espec_v2(data, identify=_ident_any)
-        except IndexError:
-            print("Reading version 2 failed. Falling back on version 1.")
-            return _espec_v1(data)
+            print("Reading version 4 failed. Falling back on version 3.")
+            return _espec_v3(data, options)
+        except KeyError:
+            try:
+                print("Reading version 3 failed. Falling back on version 2.")
+                return _espec_v2(data, identify=_ident_any)
+            except IndexError:
+                print("Reading version 2 failed. Falling back on version 1.")
+                return _espec_v1(data)
 
 if __name__ == "__main__":
     fig, ax = makeespec(argv[1], {'colorbars':['Blues','Blues','Reds']})
