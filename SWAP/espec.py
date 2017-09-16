6cd0370087820b238c4d3cd3bce000ec6e0d7f5f
Nathan Barnes <npbarnes@alaska.edu>
2017-09-16 15:43:37 -0800

    Mother of all plots

--- SWAP/espec.py
+++ SWAP/espec.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
-from matplotlib import rcParams
+from matplotlib import rcParams, gridspec
+import colormaps as cmaps
+plt.register_cmap(name='viridis', cmap=cmaps.viridis)
+plt.register_cmap(name='plasma', cmap=cmaps.plasma)
 from scipy.io import readsav
 from sys import argv
 from numpy.ma import masked_array
@@ -10,13 +13,14 @@ from mom_trans_pluto import mom_trans
 import numpy as np
 import spiceypy as sp
 import NH_tools
+from HybridReader2 import HybridReader2 as hr
 
 def _espec_v1(data):
     x = data['xpos']
     ebins = data['ebins']
     cnt = data['cnt_arr']
 
-    plt.pcolormesh(x,y,cnt, norm=LogNorm())
+    plt.pcolormesh(x,y,cnt,  norm=LogNorm())
     plt.yscale('log')
     plt.ylabel('Energy per charge ($\frac{eV}{q}$)')
     plt.xlabel('X position ($R_p$)')
@@ -118,7 +122,9 @@ def _espec_v4(data, options=None):
     # Load data from IDL
     spectrograms_by_species = data['spectrograms_by_species']
     t = data['times']
-    x = data['positions'][0,:]/1187
+    x = data['positions'][0,:]
+    y = data['positions'][1,:]
+    z = data['positions'][2,:]
     o = data['orientations']
     ebins = data['ebins']
 
@@ -135,9 +141,22 @@ def _espec_v4(data, options=None):
     mCH4 = masked_array(cnt_arr, mask=~logical_and(CH4>H,CH4>He))
     
     # Setup subplot and colorbar axes
-    fig, (ax_spec, ax_theta, ax_phi, ax_spin) = plt.subplots(4, sharex=True)
-    fig.subplots_adjust(right=0.8)
-    fig.subplots_adjust(hspace=0.05)
+#    fig = plt.figure(figsize = (rcParams['figure.figsize'][0], 2*rcParams['figure.figsize'][1]))
+#    gs = gridspec.GridSpec(8,1)
+#    ax_spec  = plt.subplot(gs[0,0])
+#    ax_theta = plt.subplot(gs[1,0], sharex=ax_spec)
+#    ax_phi   = plt.subplot(gs[2,0], sharex=ax_spec)
+#    ax_spin  = plt.subplot(gs[3,0], sharex=ax_spec)
+#
+#    ax_xy    = plt.subplot(gs[4:6,0])
+#    ax_xz    = plt.subplot(gs[6:8,0], sharex=ax_xy)
+
+
+    fig, (ax_spec, ax_theta, ax_phi, ax_spin, ax_xy, ax_xz) = plt.subplots(nrows=6, sharex=True, 
+            gridspec_kw={'height_ratios':[1,1,1,1,2,3]}, 
+            figsize=(rcParams['figure.figsize'][0], 2.25*rcParams['figure.figsize'][1]))
+
+    fig.subplots_adjust(right=0.8, hspace=0.05)
 
     spec_pos = ax_spec.get_position()
     cbar_CH4 = fig.add_axes([spec_pos.x1+.01, spec_pos.y0+.01, 0.1/3, spec_pos.height-.02])
@@ -169,7 +188,49 @@ def _espec_v4(data, options=None):
     ax_phi.set_ylim(-180,180)
     ax_spin.set_ylim(-180,180)
 
+    # Plot positions
+    h = hr('/home/nathan/data/2017-Wed-Aug-30/pluto-1/databig','np')
+    n = h.get_last_timestep()[-1]
+
+    # Get grid spacing
+    qx = h.para['qx']
+    qy = h.para['qy']
+    qzrange = h.para['qzrange']
+
+    # Find the center index of the grid
+    cx = h.para['nx']/2
+    cy = h.para['ny']/2
+    cz = h.para['zrange']/2
+
+    # the offset of pluto from the center isn't always availible
+    try:
+        po = h.para['pluto_offset']
+    except KeyError:
+        print("Couldn't get pluto_offset. It has been assumed to be 30, but it probably isn't.")
+        po = 30
+
+    # Set constatnt for Pluto radius 
+    Rp = 1187. # km
+
+    # Shift grid so that Pluto lies at (0,0,0) and convert from km to Rp
+    qx = (qx - qx[len(qx)/2 + po])/Rp
+    qy = (qy - qy[len(qy)/2])/Rp
+    qzrange = (qzrange - qzrange[len(qzrange)/2])/Rp
+
+    qt = [NH_tools.time_at_pos(x*Rp) for x in qx]
+
+    T,Y = np.meshgrid(qt,qy)
+    ax_xy.pcolormesh(T,Y,n[:,:,cz].transpose(), cmap='viridis', norm=LogNorm())
+
+    T,Z = np.meshgrid(qt,qzrange)
+    ax_xz.pcolormesh(T,Z,n[:,cy,:].transpose(), cmap='viridis', norm=LogNorm())
+
+    ax_xy.plot(t, y/Rp-np.max(qy), color='black', linewidth=2)
+    ax_xz.plot(t, np.zeros(z.shape), color='black', linewidth=2)
+
     # Set title, labels and adjust figure
+    ax_spec.set_xlim(np.min(t), np.max(t))
+    ax_spec.set_ylim(5e1, 1e4)
     ax_spec.set_title("Synthetic SWAP Energy Spectrogram", y=1.4)
     Hecb.ax.set_title('COIN (Hz)', fontdict={'fontsize':'small'})
     ax_spec.set_yscale('log')
@@ -177,9 +238,14 @@ def _espec_v4(data, options=None):
     ax_theta.set_ylabel('Sun Theta\nAngle (deg)')
     ax_phi.set_ylabel('Sun Phi\nAngle (deg)')
     ax_spin.set_ylabel('Spin\nAngle (deg)')
-    ax_spin.set_xlabel('Time (UTC)')
+    ax_xz.set_xlabel('Time (UTC)')
 
-    ax_spin.set_xticklabels([sp.timout(et, 'HR:MN:SC') for et in ax_spec.get_xticks()], rotation=-20, horizontalalignment='left')
+    ax_xy.set_ylabel('Y ($R_p$)')
+    ax_xz.set_ylabel('Z ($R_p$)')
+    ax_xy.set_ylim(np.min(qy), np.max(qy))
+    ax_xz.set_ylim(1.5*np.min(qy), 1.5*np.max(qy))
+
+    ax_xz.set_xticklabels([sp.timout(et, 'HR:MN:SC') for et in ax_spec.get_xticks()], rotation=-20, horizontalalignment='left')
 
     ax_twin = ax_spec.twiny()
     ax_twin.set_xlabel('X ($R_p$)')
